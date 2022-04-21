ff7860b43f44771fc7ce9f2aeba031cb0aaf13e0
Mahendran1604 <rmahendranme@gmail.com>
2022-04-21 19:40:35 +0530

    new code

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,19 @@
+package org.emp;
+
+public class Employee {
+	public void empId(int i) {
+		System.out.println(i);
+	}
+	public void empId(String i) {
+		System.out.println(i);
+	}
+
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.empId(12345);
+		e.empId("18EC001");
+		
+		
+	}
+
+}
